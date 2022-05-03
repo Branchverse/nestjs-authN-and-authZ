04fc343c97918e63f735cc43dbd1f1e032b42926
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-05-03 11:51:35 +0200

    Remove coment

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
   app.use(cookieParser());
-  //app.useGlobalGuards(new JwtAuthGuard);
 
   // Swagger
   const config = new DocumentBuilder()
