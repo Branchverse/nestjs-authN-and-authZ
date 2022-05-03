52108df539f80d9c6562665dd388a87db8a3ff8f
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-05-03 16:41:04 +0200

    Move login dto to auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,12 @@
 import { Body, ClassSerializerInterceptor, Controller, Get, HttpCode, HttpStatus, Post, Req, Res, SerializeOptions, UseGuards, UseInterceptors } from '@nestjs/common';
-import { ApiBasicAuth, ApiBearerAuth, ApiBody, ApiCookieAuth, ApiHeader, ApiHeaders, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { Response } from 'express';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { LoginUserDto } from '../users/dto/login-user.dto';
 import { User } from '../users/entities/user.entity';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './guards/jwtAuth.guard';
+import { LoginUserDto } from './dto/login-user.dto';
 import { LocalAuthGuard } from './guards/localAuth.guard';
-import { OptionalJwtAuthGuard } from './guards/optionalJwtAuth.guard';
 import { Public } from './guards/public.guard';
 import { RequestWithUser } from './interfaces/requestWithUser.interface';
 
@@ -21,7 +19,7 @@ export class AuthController {
         private readonly authService: AuthService
     ) { }
 
-    @ApiResponse({ status: HttpStatus.OK, description: 'User authenticated' , type: User})
+    @ApiResponse({ status: HttpStatus.OK, description: 'User authenticated', type: User })
     @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Cookie is not valid' })
     @Get()
     authenticate(@Req() request: RequestWithUser) {
@@ -39,7 +37,7 @@ export class AuthController {
     @Public()
     @HttpCode(200)
     @UseGuards(LocalAuthGuard)
-    @ApiResponse({ status: HttpStatus.OK, description: 'User logged in', type: User})
+    @ApiResponse({ status: HttpStatus.OK, description: 'User logged in', type: User })
     @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Credentials' })
     @Post('login')
     async logIn(@Req() request: RequestWithUser, @Res() response: Response, @Body() loginUserDto: LoginUserDto) {
